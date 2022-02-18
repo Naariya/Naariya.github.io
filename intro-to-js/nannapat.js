c9ff56c23f92fa0fbb52faea5b6787cdef3c60fa
Nannapat Ariyakornwijit <nannapat9858@gmail.com>
2022-02-17 22:49:34 -0800

    add name and province

--- intro-to-js/nannapat.js
+++ intro-to-js/nannapat.js
@@ -4,9 +4,22 @@ function askMyName() {
     if (name != null) {
         document.getElementById("myname").innerHTML = "My name is " + name;
     }
- }
- 
- function helloWorld() {
- const h1message = document.getElementById("myname").innerHTML
+}
+
+function helloWorld() {
+    const h1message = document.getElementById("myname").innerHTML
     alert(h1message + ' เมาเหล้าเสียหลัก เมารักเสียใจ')
- }
+}
+
+function calAge() {
+    const year = prompt("Tell me your year of birth? (please put it in number.)");
+    const age = (2022 - year)
+    alert("Congratulations, You have been the citizen of the world for" + " " + age + " years.")
+}
+
+function loadProfile() {
+    const firstname = "Nannapat"
+    const province = "Bangkok"
+    document.getElementById("my-name").innerHTML = firstname;
+    document.getElementById("my-province").innerHTML = province;
+}
